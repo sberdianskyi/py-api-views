4220cec83082a532e36e76b7f72094284354bc92
Serhii Berdianskyi <zeferot94@gmail.com>
2024-11-03 18:28:38 +0200

    Update

--- cinema/models.py
+++ cinema/models.py
@@ -22,7 +22,7 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     def __str__(self):
-        return {self.name}
+        return self.name
 
 
 class Movie(models.Model):
